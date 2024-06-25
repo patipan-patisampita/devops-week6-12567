aba168beedff93584028bcec78d2a4eb7657b3b0
patipan <patipan.patisampita@gmail.com>
2024-06-25 13:39:19 +0700

    update

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+const App = () => {
+  return (
+    <>
+      <h1 style={{ color: "red", textAlign: "center" }}>Welcome to DevOps</h1>
+      <hr />
+      <div style={{ textAlign: "center" }}>
+        <h2>My Name is: Mr.Jhon Doe</h2>
+        <img
+          src="https://cdn.pixabay.com/photo/2019/06/02/17/33/woman-4246954_1280.jpg"
+          width={350}
+          height={250}
+          alt=""
+        />
+      </div>
+    </>
+  );
+};
+
+export default App;
